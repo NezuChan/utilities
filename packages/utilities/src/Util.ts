f9f4ba631962ef9bd8757fec7ab113cd149d41bd
kagchi <yonuxbot@gmail.com>
2023-03-14 18:49:04 +0700

    feat: add text decoder & encoder

--- packages/utilities/src/Util.ts
+++ packages/utilities/src/Util.ts
@@ -1,4 +1,5 @@
 import fs from "node:fs";
+import { decode, encode } from "base65536";
 
 export class Util {
     public static formatDate(dateFormat: Intl.DateTimeFormat, date: Date | number = new Date()): string {
@@ -11,6 +12,15 @@ export class Util {
 
     public static loadJSON<T>(path: string): T {
         return JSON.parse(fs.readFileSync(new URL(path, import.meta.url)) as unknown as string) as T;
+    }
+
+    public static decodeBase65536<T>(bufferOrString: Uint8Array | string): T {
+        if (typeof bufferOrString === "string") bufferOrString = decode(bufferOrString);
+        return JSON.parse(Buffer.from(bufferOrString.buffer, bufferOrString.byteOffset, bufferOrString.byteLength).toString("utf-8")) as T;
+    }
+
+    public static encodeBase65536(str: string): string {
+        return encode(Buffer.from(str));
     }
 
     public static date(): string {
