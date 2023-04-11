19cf8db2be13f5bbbac736cfcaa26a59def7c48c
kagchi <yonuxbot@gmail.com>
2023-04-11 17:37:24 +0700

    fix: stringify limit enum

--- packages/constants/src/Limit.ts
+++ packages/constants/src/Limit.ts
@@ -1,18 +1,18 @@
 export enum QueueLimit {
-    FREE = 350,
-    USER_TIER_1 = 512,
-    GUILD_TIER_1 = 320,
-    TIER_2 = Number.MAX_SAFE_INTEGER
+    "FREE" = 350,
+    "USER_TIER_1" = 512,
+    "GUILD_TIER_1" = 320,
+    "TIER_2" = Number.MAX_SAFE_INTEGER
 }
 
 export enum PlaylistLimit {
-    FREE = 5,
-    TIER_1 = 10,
-    TIER_2 = 30
+    "FREE" = 5,
+    "TIER_1" = 10,
+    "TIER_2" = 30
 }
 
 export enum TrackLimit {
-    FREE = 20,
-    TIER_1 = 40,
-    TIER_2 = 70
+    "FREE" = 20,
+    "TIER_1" = 40,
+    "TIER_2" = 70
 }
