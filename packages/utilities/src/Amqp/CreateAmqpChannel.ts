0bb576b6b9ad3ddff6cc0936cf10ba96827e5768
kagchi <yonuxbot@gmail.com>
2023-05-04 17:38:00 +0700

    feat: add channelOptions

--- packages/utilities/src/Amqp/CreateAmqpChannel.ts
+++ packages/utilities/src/Amqp/CreateAmqpChannel.ts
@@ -1,6 +1,10 @@
-import amqplib, { AmqpConnectionManagerOptions, ChannelWrapper, ConnectionUrl } from "amqp-connection-manager";
+import amqplib, { AmqpConnectionManagerOptions, ChannelWrapper, ConnectionUrl, CreateChannelOpts } from "amqp-connection-manager";
 
-export function createAmqpChannel(urls: ConnectionUrl | ConnectionUrl[] | null | undefined, options?: AmqpConnectionManagerOptions | undefined): ChannelWrapper {
+export function createAmqpChannel(
+    urls: ConnectionUrl | ConnectionUrl[] | null | undefined,
+    channelOptions?: CreateChannelOpts | undefined,
+    options?: AmqpConnectionManagerOptions | undefined
+): ChannelWrapper {
     const connection = amqplib.connect(urls, options);
-    return connection.createChannel();
+    return connection.createChannel(channelOptions);
 }
