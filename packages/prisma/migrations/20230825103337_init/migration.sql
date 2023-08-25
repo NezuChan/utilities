389158d06f3c77a4a495e8c6453de6e26cbfb81d
kagchi <yonuxbot@gmail.com>
2023-08-25 17:34:14 +0700

    feat: update prisma schema

--- packages/prisma/migrations/20230825103337_init/migration.sql
+++ packages/prisma/migrations/20230825103337_init/migration.sql
@@ -1,4 +1,7 @@
 -- CreateEnum
+CREATE TYPE "PlaylistVisibility" AS ENUM ('PUBLIC', 'PRIVATE');
+
+-- CreateEnum
 CREATE TYPE "CaseType" AS ENUM ('BAN', 'KICK', 'MUTE', 'UNBAN', 'UNMUTE', 'WARN');
 
 -- CreateEnum
@@ -12,10 +15,13 @@ CREATE TYPE "PremiumType" AS ENUM ('OTHER', 'PATREON', 'GIVEAWAY');
 
 -- CreateTable
 CREATE TABLE "Guild" (
+    "id" TEXT NOT NULL,
     "guildId" TEXT NOT NULL,
     "clientId" TEXT NOT NULL,
     "locale" TEXT NOT NULL DEFAULT 'en-US',
     "prefix" TEXT NOT NULL DEFAULT 'n!',
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
     "premiumTierType" "TierType" NOT NULL DEFAULT 'FREE',
     "premiumActivatedBy" TEXT,
 
@@ -23,30 +29,23 @@ CREATE TABLE "Guild" (
 );
 
 -- CreateTable
-CREATE TABLE "Instance" (
-    "instanceId" TEXT NOT NULL,
-    "guildId" TEXT NOT NULL,
-    "userId" TEXT NOT NULL,
-    "type" TEXT NOT NULL,
-    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-
-    CONSTRAINT "Instance_pkey" PRIMARY KEY ("instanceId")
-);
-
--- CreateTable
 CREATE TABLE "TempVoice" (
+    "id" TEXT NOT NULL,
     "guildId" TEXT NOT NULL,
     "clientId" TEXT NOT NULL,
     "channelId" TEXT NOT NULL,
-    "parentChannelId" TEXT NOT NULL,
+    "parentId" TEXT NOT NULL,
     "ownerId" TEXT NOT NULL,
     "taskId" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
 
     CONSTRAINT "TempVoice_pkey" PRIMARY KEY ("guildId","clientId")
 );
 
 -- CreateTable
 CREATE TABLE "Case" (
+    "id" TEXT NOT NULL,
     "guildId" TEXT NOT NULL,
     "clientId" TEXT NOT NULL,
     "caseId" VARCHAR NOT NULL,
@@ -54,115 +53,146 @@ CREATE TABLE "Case" (
     "reason" TEXT NOT NULL,
     "executorId" TEXT NOT NULL,
     "type" "CaseType" NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
 
     CONSTRAINT "Case_pkey" PRIMARY KEY ("guildId","clientId")
 );
 
 -- CreateTable
 CREATE TABLE "DJRole" (
+    "id" TEXT NOT NULL,
     "roleId" VARCHAR NOT NULL,
     "authorId" TEXT NOT NULL,
     "guildId" TEXT NOT NULL,
     "clientId" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
 
     CONSTRAINT "DJRole_pkey" PRIMARY KEY ("guildId","clientId","roleId")
 );
 
 -- CreateTable
 CREATE TABLE "DJUser" (
+    "id" TEXT NOT NULL,
     "userId" TEXT NOT NULL,
     "authorId" TEXT NOT NULL,
     "guildId" TEXT NOT NULL,
     "clientId" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
 
     CONSTRAINT "DJUser_pkey" PRIMARY KEY ("guildId","clientId","userId")
 );
 
 -- CreateTable
 CREATE TABLE "defaultVolume" (
+    "id" TEXT NOT NULL,
     "guildId" TEXT NOT NULL,
     "clientId" TEXT NOT NULL,
     "activatedBy" TEXT NOT NULL,
     "defaultVolume" INTEGER NOT NULL DEFAULT 100,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
 
     CONSTRAINT "defaultVolume_pkey" PRIMARY KEY ("guildId","clientId")
 );
 
 -- CreateTable
 CREATE TABLE "clearQueueAfterEnd" (
+    "id" TEXT NOT NULL,
     "guildId" TEXT NOT NULL,
     "clientId" TEXT NOT NULL,
     "activatedBy" TEXT NOT NULL,
     "enabled" BOOLEAN NOT NULL DEFAULT true,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
 
     CONSTRAINT "clearQueueAfterEnd_pkey" PRIMARY KEY ("guildId","clientId")
 );
 
 -- CreateTable
 CREATE TABLE "DJMode" (
+    "id" TEXT NOT NULL,
     "guildId" TEXT NOT NULL,
     "clientId" TEXT NOT NULL,
     "activatedBy" TEXT NOT NULL,
     "enabled" BOOLEAN NOT NULL DEFAULT false,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
 
     CONSTRAINT "DJMode_pkey" PRIMARY KEY ("guildId","clientId")
 );
 
 -- CreateTable
 CREATE TABLE "autoPlay" (
+    "id" TEXT NOT NULL,
     "guildId" TEXT NOT NULL,
     "clientId" TEXT NOT NULL,
     "activatedBy" TEXT NOT NULL,
     "enabled" BOOLEAN NOT NULL DEFAULT false,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
 
     CONSTRAINT "autoPlay_pkey" PRIMARY KEY ("guildId","clientId")
 );
 
 -- CreateTable
 CREATE TABLE "announceTrackStart" (
+    "id" TEXT NOT NULL,
     "guildId" TEXT NOT NULL,
     "clientId" TEXT NOT NULL,
     "activatedBy" TEXT NOT NULL,
     "enabled" BOOLEAN NOT NULL DEFAULT true,
     "channel" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
 
     CONSTRAINT "announceTrackStart_pkey" PRIMARY KEY ("guildId","clientId")
 );
 
 -- CreateTable
 CREATE TABLE "StayInVc" (
+    "id" TEXT NOT NULL,
     "guildId" TEXT NOT NULL,
     "clientId" TEXT NOT NULL,
     "activatedBy" TEXT NOT NULL,
     "enabled" BOOLEAN NOT NULL DEFAULT false,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
 
     CONSTRAINT "StayInVc_pkey" PRIMARY KEY ("guildId","clientId")
 );
 
 -- CreateTable
 CREATE TABLE "Invite" (
-    "id" INTEGER NOT NULL,
+    "id" TEXT NOT NULL,
     "guildId" TEXT NOT NULL,
     "clientId" TEXT NOT NULL,
     "enabled" BOOLEAN NOT NULL DEFAULT false,
     "adminBypass" BOOLEAN NOT NULL DEFAULT false,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
 
     CONSTRAINT "Invite_pkey" PRIMARY KEY ("guildId","clientId")
 );
 
 -- CreateTable
 CREATE TABLE "Phising" (
-    "id" INTEGER NOT NULL,
+    "id" TEXT NOT NULL,
     "guildId" TEXT NOT NULL,
     "clientId" TEXT NOT NULL,
     "enabled" BOOLEAN NOT NULL DEFAULT false,
     "level" "PhisingLevel" NOT NULL DEFAULT 'MEDIUM',
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
 
     CONSTRAINT "Phising_pkey" PRIMARY KEY ("guildId","clientId")
 );
 
 -- CreateTable
 CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
     "userId" TEXT NOT NULL,
     "email" TEXT,
     "patreonUserId" TEXT,
@@ -172,35 +202,38 @@ CREATE TABLE "User" (
     "additionalPlaylistsSize" INTEGER NOT NULL DEFAULT 0,
     "tier1Tickets" INTEGER NOT NULL DEFAULT 0,
     "tier2Tickets" INTEGER NOT NULL DEFAULT 0,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
 
     CONSTRAINT "User_pkey" PRIMARY KEY ("userId")
 );
 
 -- CreateTable
 CREATE TABLE "Playlist" (
+    "id" TEXT NOT NULL,
     "playlistId" VARCHAR NOT NULL,
     "userId" TEXT NOT NULL,
     "playlistName" TEXT NOT NULL,
     "trackLimit" INTEGER NOT NULL DEFAULT 20,
+    "visibility" "PlaylistVisibility" NOT NULL DEFAULT 'PUBLIC',
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
 
     CONSTRAINT "Playlist_pkey" PRIMARY KEY ("playlistId")
 );
 
 -- CreateTable
 CREATE TABLE "PlaylistTrack" (
+    "id" TEXT NOT NULL,
     "trackId" VARCHAR NOT NULL,
     "userId" TEXT NOT NULL,
     "playlistId" TEXT NOT NULL,
     "track" JSONB NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
 
     CONSTRAINT "PlaylistTrack_pkey" PRIMARY KEY ("trackId")
 );
-
--- CreateIndex
-CREATE UNIQUE INDEX "Phising_guildId_key" ON "Phising"("guildId");
-
--- AddForeignKey
-ALTER TABLE "Instance" ADD CONSTRAINT "Instance_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("userId") ON DELETE CASCADE ON UPDATE NO ACTION;
 
 -- AddForeignKey
 ALTER TABLE "TempVoice" ADD CONSTRAINT "TempVoice_guildId_clientId_fkey" FOREIGN KEY ("guildId", "clientId") REFERENCES "Guild"("guildId", "clientId") ON DELETE CASCADE ON UPDATE NO ACTION;
