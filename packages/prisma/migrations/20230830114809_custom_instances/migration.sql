8c644031455fb10ac2641c67d4b27fdd91285a63
kagchi <yonuxbot@gmail.com>
2023-08-30 18:48:54 +0700

    fix: add missing containerId

--- packages/prisma/migrations/20230830114809_custom_instances/migration.sql
+++ packages/prisma/migrations/20230830114809_custom_instances/migration.sql
@@ -3,6 +3,7 @@ CREATE TABLE "Instance" (
     "id" TEXT NOT NULL,
     "userId" TEXT NOT NULL,
     "botId" TEXT NOT NULL,
+    "containerId" TEXT NOT NULL,
     "memoryLimit" TEXT NOT NULL DEFAULT '128',
     "type" TEXT NOT NULL,
     "node" TEXT NOT NULL,
