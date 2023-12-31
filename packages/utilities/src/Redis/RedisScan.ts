b9dc0016dd4982b07a84df49b8740fe98e200b55
Hazmi Alfarizqi <contact@hzmi.xyz>
2023-12-31 22:23:10 +0700

    fix(redisScan): fix redis cluster

--- packages/utilities/src/Redis/RedisScan.ts
+++ packages/utilities/src/Redis/RedisScan.ts
@@ -1,12 +1,23 @@
-import { Redis, Cluster } from "ioredis";
+import { Redis, Cluster, NodeRole } from "ioredis";
 
-export function redisScan(redis: Cluster | Redis, key: string, match: string, count: number): Promise<string[]> {
+export function redisScan(redis: Cluster | Redis, key: string, count: number, nodeRole: NodeRole = "slave"): Promise<string[]> {
     return new Promise((resolve, reject) => {
         const keys: string[] = [];
-        redis.sscanStream(key, { match, count })
-            .on("data", (keyChunk: string[]) => keys.push(...keyChunk))
-            .on("end", () => resolve(keys))
-            .on("error", reject);
+
+        if (redis instanceof Cluster) {
+            const results = redis.nodes(nodeRole).map(node => redisScan(node, key, count, nodeRole));
+            Promise.all(results)
+                .then(res => {
+                    res.forEach(result => keys.push(...result));
+                    resolve(keys);
+                })
+                .catch(reject);
+        } else {
+            redis.scanStream({ match: `${key}*`, count })
+                .on("data", (keyChunk: string[]) => keys.push(...keyChunk))
+                .on("end", () => resolve(keys))
+                .on("error", reject);
+        }
     });
 }
 
