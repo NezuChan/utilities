a2168223d9b4080f2d0aeeb53154641cefc1574d
hzmi <contact@hzmi.xyz>
2024-02-23 13:24:30 +0000

    feat!: revamp amqp queues

--- packages/constants/src/RabbitMQ.ts
+++ packages/constants/src/RabbitMQ.ts
@@ -2,18 +2,25 @@ export enum RabbitMQ {
     PLAYER_EXCHANGE = "nezu-player",
     PLAYER_RECV = "nezu-player.recv",
     PLAYER_SEND = "nezu-player.send",
+
     TASKS_SEND = "scheduled-tasks.send",
     TASKS_RECV = "scheduled-tasks.recv",
     TASKS_EXCHANGE = "scheduled-tasks",
+
     API_RECV = "nezu-api.recv",
     API_SEND = "nezu-api.send",
     API_EXCHANGE = "nezu-api",
+
     BOT_RPC = "nezu-bot.rpc",
-    GATEWAY_EXCHANGE = "nezu-gateway",
-    GATEWAY_QUEUE_RECV = "nezu-gateway.recv",
-    GATEWAY_QUEUE_SEND = "nezu-gateway.send",
-    GATEWAY_QUEUE_STATS = "nezu-gateway.stats",
-    GATEWAY_QUEUE_STATS_POOLER = "nezu-gateway.stats_pooler"
+
+    GATEWAY_EXCHANGE = "kanao-gateway"
+}
+
+export enum GatewayExchangeRoutes {
+    DISPATCH = "dispatch",
+    RECEIVE = "receive",
+    CACHE = "cache",
+    SEND = "send"
 }
 
 export enum RabbitMQRpc {
