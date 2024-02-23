365e9cf118f9607e205d614de7f2e9317721be04
hzmi <contact@hzmi.xyz>
2024-02-23 20:31:00 +0700

    feat!: revamp amqp queues (#214)

--- packages/utilities/src/index.ts
+++ packages/utilities/src/index.ts
@@ -5,4 +5,4 @@ export * from "./Redis/CreateRedis.js";
 export * from "./Redis/GenKey.js";
 export * from "./Redis/CreateRedis.js";
 export * from "./Amqp/CreateAmqpChannel.js";
-export * from "./Amqp/GenRoute.js";
+export * from "./Amqp/RoutedQueue.js";
